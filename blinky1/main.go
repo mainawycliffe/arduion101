0ae2dabc4237c70a711fd90cbd00b0e9596e32a1
Maina Wycliffe <wmmaina7@gmail.com>
2020-05-27 00:47:39 +0300

    Fix Formatting and Linting Issues

--- blinky1/main.go
+++ blinky1/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "machine"
-    "time"
+	"machine"
+	"time"
 )
 
 func main() {
 
-    // use pin 13, which is connected to the built-in LED
-    var led machine.Pin = machine.Pin(13)
+	// use pin 13, which is connected to the built-in LED
+	var led machine.Pin = machine.Pin(13)
 
-    // set the pin to output mode
-    led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	// set the pin to output mode
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-    for {
-        led.High() // pull high (output power, light up led)
-        time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
-        led.Low() // pull low (cut off power)
-        time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
-    }
+	for {
+		led.High()                         // pull high (output power, light up led)
+		time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
+		led.Low()                          // pull low (cut off power)
+		time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
+	}
 }
--- blinky2/main.go
+++ blinky2/main.go
@@ -1,31 +1,31 @@
-package main 
+package main
 
 import (
-    "machine"
-    "time"
+	"machine"
+	"time"
 )
 
 func main() {
 
-    led := machine.Pin(13)
-    // same as var led = machine.LED
+	led := machine.Pin(13)
 
-    led.Configure(machine.PinConfig{1})
-    // 1 = output mode, 0 = input mode
+	// same as var led = machine.LED
+	led.Configure(machine.PinConfig{1})
+	// 1 = output mode, 0 = input mode
 
-    led_switch := false // led switch
+	led_switch := false // led switch
 
-    for {
-        led.Set(led_switch)
-        led_switch = !led_switch // invert switch
-        if led_switch {
-            delay(1000)
-        } else {
-            delay(500)
-        }
-    }
+	for {
+		led.Set(led_switch)
+		led_switch = !led_switch // invert switch
+		if led_switch {
+			delay(1000)
+		} else {
+			delay(500)
+		}
+	}
 }
 
 func delay(t int64) { // time delay function
-    time.Sleep(time.Duration(1000000 * t))
+	time.Sleep(time.Duration(1000000 * t))
 }
