8eef52182d6454f82b79ff4e6aa5c93897a48aac
Maina Wycliffe <wmmaina7@gmail.com>
2020-05-26 18:00:12 +0300

    Initial Project Setuo with Basic Examples

---
+++ blinky1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "machine"
+    "time"
+)
+
+func main() {
+
+    // use pin 13, which is connected to the built-in LED
+    var led machine.Pin = machine.Pin(13)
+
+    // set the pin to output mode
+    led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+    for {
+        led.High() // pull high (output power, light up led)
+        time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
+        led.Low() // pull low (cut off power)
+        time.Sleep(time.Millisecond * 500) // wait 500 milliseconds
+    }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mainawycliffe/arduion101
+
+go 1.14
